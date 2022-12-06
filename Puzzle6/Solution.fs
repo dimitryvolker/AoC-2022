d89d57d7a451eb87962e890e97cc23af87777524
Dimitry Volker <dimitry.volker@gmail.com>
2022-12-06 16:30:39 +0100

    Removed old solution and changed it to recursive

--- Puzzle6/Solution.fs
+++ Puzzle6/Solution.fs
@@ -1,19 +1,12 @@
 module AoC2022.Puzzle6
 open System.IO
 
-let findMarker (input: list<list<char>>,i: int): int = 
-    let mutable result = 0;
-    let mutable index = 0;
-    let mutable cancelationToken = true;
-
-    while cancelationToken do 
-        let distinct = input[index] |> List.distinct
-        if input[index].Length = distinct.Length then result <- index + i;  cancelationToken <- false
-
-        index <- index + 1
-
-    result
-
+let rec find(input: list<list<char>>, window: int, offset: int): int =
+    let distinct = input[offset] |> List.distinct
+    if input[offset].Length = distinct.Length then
+        (offset + window)
+    else
+        find(input, window, (offset + 1))
 
 let solve() = 
     printfn "Day 6: Tuning Trouble  --"
@@ -24,7 +17,7 @@ let solve() =
         |> Seq.toList
 
     let startSignal = input |> List.windowed 4
-    printfn "\t First start-of-packet marker on charater number: %d" (findMarker(startSignal, 4))
+    printfn "\t First start-of-packet marker on charater number: %d" (find(startSignal, 4, 0))
     
     let startSignal = input |> List.windowed 14
-    printfn "\t First start-of-message marker on charater number: %d" (findMarker(startSignal, 14))
+    printfn "\t First start-of-message marker on charater number: %d" (find(startSignal, 14, 0))
